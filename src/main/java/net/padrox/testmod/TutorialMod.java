ffe64f3ada1bf2f5b2a09cacf5479ded25029957
Padrox <antoinevollet.pro@gmail.com>
2023-06-30 01:53:10 +0200

    Clear comments

--- src/main/java/net/padrox/testmod/TutorialMod.java
+++ src/main/java/net/padrox/testmod/TutorialMod.java
@@ -16,9 +16,7 @@ import org.slf4j.Logger;
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "testmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public TutorialMod() {
@@ -38,7 +36,6 @@ public class TutorialMod
 
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
         @SubscribeEvent
